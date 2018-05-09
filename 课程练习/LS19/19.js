dc114734bdfc61b7833f9c3efacd2a0d94421225
zhangshengran <1286693585@qq.com>
2018-05-09 22:33:22 +0800

    5.9

---
+++ 课程练习/LS19/19.js
@@ -0,0 +1,31 @@
+var date1 = new Date(2017,9,18,12,34,1);//注意：月0-11，日：1-31，时：0-23，分：0-59，秒：0-59，毫秒：0-999
+console.log(date1);
+var date2 = new Date(17,9,18,12,34,1);//若years为2位的话自动加1900
+console.log(date2);
+var date3 = new Date("2017-08-09");//注意日期的格式 此处的08代表8月还是9月，对比上一个创建形式
+console.log(date3);
+var date4 = new Date(1000); //1970-01-01:00:00:01
+console.log(date4);//逆运算
+
+var date5 = new Date();//new Date(Date.now());
+console.log(date5);
+
+
+//有无new的区别
+var d1 = new Date();
+var d2 = Date();
+console.log(d1,typeof d1);//object
+console.log(d2,typeof d2);//string
+//Date原型方法 getter和setter相关
+var d = new Date("1978-11-25");
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
+console.log(d.getTimezoneOffset());
+d.setDate(11);
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
+d.setFullYear(1999,5,3);
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
+//Date原型方法 转成字符串相关
+var d = new Date(2012,3,21,15,7,23,234);
+console.log(d.toTimeString(),"___",d.toLocaleTimeString());
+console.log(d.toDateString(),"___",d.toLocaleDateString());
+console.log(d.toJSON());
